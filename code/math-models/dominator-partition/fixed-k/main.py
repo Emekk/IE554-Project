b1264fe712bc092ee0317938180f08aae00a1926
Omer <1>
2025-05-28 11:56:10 +0300

    refactor dominator partition model: change K to 2, update variable types to continuous, and modify solution output format

--- code/math-models/dominator-partition/fixed-k/main.py
+++ code/math-models/dominator-partition/fixed-k/main.py
@@ -7,16 +7,17 @@ from draw_graph import draw_graph
 
 
 # parameters
+SEARCH_FESAIBLE = False
 V, E = tree3
-K = 3
+K = 2
 PI = {i for i in range(1, K+1)}  # Number of blocks (fixed)
 
 # model
 m = gp.Model('dominator-partition-fixed-k')
 
 # decision variables
-x = m.addVars(V, PI, vtype=GRB.BINARY, lb=0, ub=1, name="x")  # x[v, i]
-d = m.addVars(V, PI, vtype=GRB.BINARY, lb=0, ub=1, name="d")  # d[v, i]
+x = m.addVars(V, PI, vtype=GRB.CONTINUOUS, lb=0, ub=1, name="x")  # x[v, i]
+d = m.addVars(V, PI, vtype=GRB.CONTINUOUS, lb=0, ub=1, name="d")  # d[v, i]
 
 # objective: minimize number of blocks used
 m.setObjective(0, GRB.MINIMIZE)
@@ -32,46 +33,66 @@ for i in PI:
 # domination condition
 for v in V:
     for u in V:
-        if {v, u} not in E:
+        if ({v, u} not in E) and (v != u):
             for i in PI:
                 m.addConstr(x[u, i] + d[v, i] <= 1, name=f"Dominate_{v}_{u}_{i}")
 
 # each vertex dominates at least one block
 for v in V:
-    m.addConstr(gp.quicksum(d[v, i] for i in PI) >= 1, name=f"DominateBlock_{v}")
+    m.addConstr(gp.quicksum(d[v, i] for i in PI) == 1, name=f"DominateBlock_{v}")
 
 # blocks are used in order
 for i in PI.difference({K}):
     m.addConstr(gp.quicksum(x[v, i] for v in V) >= gp.quicksum(x[v, i + 1] for v in V), name=f"Order_{i}")
 
 # run the model
-m.setParam(GRB.Param.PoolSearchMode, 2)
-m.setParam(GRB.Param.PoolSolutions, 20)
+if SEARCH_FESAIBLE:
+    m.setParam(GRB.Param.PoolSearchMode, 2)
+    m.setParam(GRB.Param.PoolSolutions, 20)
 m.optimize()
+
 
 # display solution
 script_dir = os.path.dirname(os.path.abspath(__file__))
 partitions = []
 with open(f"{script_dir}/solution.txt", "w", encoding="utf-8") as f:
-    if m.SolCount > 0:
-        print(f"Found {m.SolCount} solutions.\n", file=f)
-        for solNum in range(m.SolCount):
-            m.setParam(GRB.Param.SolutionNumber, solNum)
-            print(f"Solution {solNum + 1}", file=f)
-            # print decision variables
-            for i in PI:
-                partition = []
-                for v in V:
-                    if x[v, i].Xn > 0:
-                        print(f"x[{v}, {i}] = {x[v, i].Xn}", file=f)
-                        partition.append(v)
-                partitions.append(partition)
-            print("---", file=f)
-            for v in V:
+    if SEARCH_FESAIBLE:
+        if m.SolCount > 0:
+            print(f"Found {m.SolCount} solutions.\n", file=f)
+            for solNum in range(m.SolCount):
+                m.setParam(GRB.Param.SolutionNumber, solNum)
+                print(f"Solution {solNum + 1}", file=f)
+                # print decision variables
                 for i in PI:
-                    if d[v, i].Xn > 0:
-                        print(f"d[{v}, {i}] = {d[v, i].Xn}", file=f)
-            print("", file=f)
+                    partition = []
+                    for v in V:
+                        if x[v, i].Xn > 0:
+                            print(f"x[{v}, {i}] = {x[v, i].Xn}", file=f)
+                            partition.append(v)
+                    partitions.append(partition)
+                print("---", file=f)
+                for v in V:
+                    for i in PI:
+                        if d[v, i].Xn > 0:
+                            print(f"d[{v}, {i}] = {d[v, i].Xn}", file=f)
+                print("", file=f)
+        else:
+            print("No feasible solution found.", file=f)
+    elif m.status == GRB.OPTIMAL:
+        for i in PI:
+            partition = []
+            for v in V:
+                if x[v, i].X > 0:
+                    print(f"x[{v}, {i}] = {x[v, i].X}", file=f)
+                    partition.append(v)
+            partitions.append(partition)
+        print("---", file=f)
+        for v in V:
+            for i in PI:
+                if d[v, i].X > 0:
+                    print(f"d[{v}, {i}] = {d[v, i].X}", file=f)
     else:
-        print("No optimal solution found.", file=f)
+        print("No feasible solution found.", file=f)
+        
 draw_graph(V, E, partitions=partitions, seed=0)
+m.write("model.lp")
